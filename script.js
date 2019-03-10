d8124597125965bab44e2d0db99495ce8dda283c
roninhaf <roninhaf@gmail.com>
2019-03-09 22:46:10 -0500

    Welcome page ready

--- script.js
+++ script.js
@@ -2,30 +2,18 @@
 
 const links = document.querySelectorAll('a');
 
-function firstCoord(){
-    links.forEach(link => {
-        link.style.transitionDuration = 5 + 's';
-        link.style.top = 100 + 'px';
-        link.style.left = 100 + 'px';
-        // console.log(window.innerHeight, window.innerWidth);
-    });
-}
-
 function newCoord(){
-    console.log(this);
     this.style.transitionDuration = Math.floor((Math.random() * 4) + 4) + 's';
     this.style.top = Math.floor(Math.random() * (window.innerHeight - this.offsetHeight)) + 'px';
     this.style.left = Math.floor(Math.random() * (window.innerWidth - this.offsetWidth)) + 'px';
-    // console.log(window.innerHeight, window.innerWidth);
-    
 }
 
-firstCoord();
+setTimeout(() => {
+    links.forEach(link => {
+        link.style.transitionDuration = '0.1s';
+        link.style.top = '233px';
+        link.style.left = '9px';
+    })
+}, 10);
 
-links.forEach(link => {
-    /* window.addEventListener('load', () => {
-        console.log('loaded');
-    });     */
-    link.addEventListener('transitionend', newCoord);
-
-})
+links.forEach(link => {link.addEventListener('transitionend', newCoord);});
