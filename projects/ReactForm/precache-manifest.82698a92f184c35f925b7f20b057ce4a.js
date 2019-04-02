245eae9217adb63dc44c3f5e2c4b4e37ce9be1a1
roninhaf <roninhaf@gmail.com>
2019-04-02 15:17:51 -0400

    Added React form

---
+++ projects/ReactForm/precache-manifest.82698a92f184c35f925b7f20b057ce4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "021f39a4c0bd259ce61b8951851a3787",
+    "url": "/projects/ReactForm/static/media/airplane.021f39a4.jpg"
+  },
+  {
+    "revision": "a98106e73f7ad5fd22f6",
+    "url": "/projects/ReactForm/static/js/runtime~main.f8c6c78f.js"
+  },
+  {
+    "revision": "e438629b9d07a1ade668",
+    "url": "/projects/ReactForm/static/js/main.8604b11c.chunk.js"
+  },
+  {
+    "revision": "5c7badd242f9ece752a2",
+    "url": "/projects/ReactForm/static/js/2.adfc45b8.chunk.js"
+  },
+  {
+    "revision": "e438629b9d07a1ade668",
+    "url": "/projects/ReactForm/static/css/main.620055d8.chunk.css"
+  },
+  {
+    "revision": "53c04cf5efade57e6b657e592e3f5b3b",
+    "url": "/projects/ReactForm/index.html"
+  }
+];
